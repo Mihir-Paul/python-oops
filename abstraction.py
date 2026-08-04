ffbb3fd5493776bbc0f7f8af3077b454886613a2
Mihir-Paul <mihirpgm@gmail.com>
2026-08-04 20:17:44 +0530

    Add EmailService class to demonstrate abstraction with email handling methods

---
+++ abstraction.py
@@ -0,0 +1,21 @@
+#Abstraction 
+class EmailService():
+    
+    def _connect(self):
+        print("Connecting to email server")
+        
+    def _authentication(self):
+        print("Authenticating....")
+        
+    def send_email(self):
+        self._connect()
+        self._authentication()
+        print("Sending email...")
+        self._disconnect()
+        
+    def _disconnect(self):
+        print("Disconnecting...")
+        
+email = EmailService()
+email.send_email()
+    
