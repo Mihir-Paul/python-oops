fa018e291e8082fc37c74831c03f3150936d8dee
Mihir Paul <mihirpgm@gmail.com>
2026-08-05 23:04:13 +0530

    Refactor BankAccount class for consistency in method formatting and improve interest method logic

--- static_method.py
+++ static_method.py
@@ -3,35 +3,30 @@
 # we use the decorator @staticmethod to define a static method 
 
 class BankAccount():
-    MIN_BALANCE = 100
-    
+    Min_Balance = 100
+
     def __init__(self,owner,balance):
         self.owner = owner 
         self._balance = balance 
-        
+
     def deposit(self,amount):
-        new_amount = amount
+        new_amount = amount 
         if self._is_valid_amount(new_amount):
-            self._balance += new_amount
+            self._balance+= new_amount 
             self.__log_transaction("Deposit",new_amount)
-            
         else:
             print("Amount must be positive")
-            
-    def _is_valid_amount(self,amount): #Protected Methods 
-            return amount>0
-        
-    def __log_transaction(self,transaction_type,amount): #Priavte Method
-        print(f"Logging {transaction_type} of {amount}. New Balance: {self._balance}")
-    
+
+    def _is_valid_amount(self,amount):
+        return amount>0
+
+    def __log_transaction(self,transaction_type,amount):
+        print(f"Logging {transaction_type} of {amount}.\nBalance: {self._balance} ")
+
     @staticmethod
     def interest(rate):
-        return 0<=rate<=10
-        
-            
-account = BankAccount("Diablo",900)
-account.deposit(200)
-
-print(BankAccount.interest(5))
-print(BankAccount.interest(100))
-
+        return 0<rate<10
+    
+account = BankAccount("Mihir",200)
+account.deposit(500)
+print(BankAccount.interest(90))
