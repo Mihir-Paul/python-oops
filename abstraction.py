7988099d0955be890c9238634200ba01d96b7e2f
Mihir Paul <mihirpgm@gmail.com>
2026-08-05 23:57:29 +0530

    Refactor EmailService and BadBankAccount classes for improved clarity and consistency in method usage

--- abstraction.py
+++ abstraction.py
@@ -1,21 +1,19 @@
 #Abstraction 
 class EmailService():
-    
     def _connect(self):
         print("Connecting to email server")
-        
+
     def _authentication(self):
-        print("Authenticating....")
-        
+        print("Authenticating...")
+
     def send_email(self):
         self._connect()
         self._authentication()
-        print("Sending email...")
+        print("Sending email..")
         self._disconnect()
-        
+
     def _disconnect(self):
         print("Disconnecting...")
-        
+
 email = EmailService()
 email.send_email()
-    
--- encapsulation.py
+++ encapsulation.py
@@ -3,35 +3,43 @@
 #Bad Programming
 class BadBankAccount():
     def __init__(self,balance):
-        self._balance = balance
-        
+        self.balance = balance
+
 account = BadBankAccount(0.0)
-account._balance = -1
-print(account._balance)
+account.balance = -1
+print(account.balance)
 
 #Programming with encapsulation
+
 class BankAccount():
     def __init__(self,balance):
         self._balance = balance 
-        
+
     @property
     def balance(self):
         return self._balance
-    
+
     def deposit(self,amount):
-        if amount<=0:
-            raise ValueError("Deposit amount must be positive")
-        self._balance+= amount
-        
+        new_amount = amount
+        if self._is_valid_amount(new_amount):
+            raise ValueError("Amount must be positive")
+        self._balance += new_amount 
+
     def withdraw(self,amount):
-        if amount<=0:
-            raise ValueError("Withdraw amount must be positive")
-        elif amount>=self._balance:
-            raise ValueError("Balance amount is less")
-        self._balance -= amount 
-        
+        new_amount = amount 
+        if self._is_valid_amount(new_amount):
+            raise ValueError("Amount must be positive")
+        elif new_amount>self._balance:
+            raise ValueError("Insufficient Balance")
+        self._balance -= new_amount
+    
+
+
+    def _is_valid_amount(self,amount):
+        return amount<0 
+    
 account = BankAccount(900)
-account.deposit(200)
+account.deposit(900)
 print(account.balance)
-account.withdraw(800)
+account.withdraw(200)
 print(account.balance)
