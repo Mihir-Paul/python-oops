30bd8db435df471d0ff8a08fe06e6138cf148575
Mihir Paul <mihirpgm@gmail.com>
2026-08-08 21:36:20 +0530

    Add Salary and Employee classes to calculate total annual salary

---
+++ composition.py
@@ -0,0 +1,19 @@
+class Salary:
+    def __init__(self,pay,bonus):
+        self.pay = pay
+        self.bonus = bonus 
+
+    def annual_salary(self):
+        return (self.pay*12)+self.bonus
+
+class Employee:
+    def __init__(self,name,age,pay,bonus):
+        self.name = name 
+        self.age = age 
+        self.salary = Salary(pay,bonus)
+
+    def total_salary(self):
+        return self.salary.annual_salary()
+
+employee1 = Employee("Michael",30,12000,6000)
+print(employee1.total_salary())
